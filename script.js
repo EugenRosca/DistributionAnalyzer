accb0eeadaa619485ef963661f2361fc0e8ba805
Eugen Roșca <99650340+EugenRosca@users.noreply.github.com>
2026-06-29 21:40:53 +0300

    Update R evaluation to use toArray method

--- script.js
+++ script.js
@@ -11,10 +11,13 @@ await webR.init();
 output.textContent = "WebR loaded successfully.";
 
 document.getElementById("runButton").addEventListener("click", async () => {
+
     output.textContent = "Running R...";
 
     const result = await webR.evalR("2 + 2");
-    const value = await result.toJs();
 
-    output.textContent = "Result from R = " + value;
+    const value = await result.toArray();
+
+    output.textContent = "Result from R = " + value[0];
+
 });
