8c50f67b971fda6219a64fa1789952214d9575ef
Eugen Roșca <99650340+EugenRosca@users.noreply.github.com>
2026-06-29 22:03:15 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,11 @@
+alert("script.js loaded");
+
 const fileInput = document.getElementById("csvFile");
 const fileName = document.getElementById("fileName");
 
 fileInput.addEventListener("change", function () {
 
-    if (this.files.length === 0) {
-
-        fileName.textContent = "No file selected";
-        return;
-
-    }
+    alert("File selected!");
 
     fileName.textContent = this.files[0].name;
 
