8207b61555fd042df7dfa7faeab3e1b2b05fc230
Eugen Roșca <99650340+EugenRosca@users.noreply.github.com>
2026-06-29 22:09:37 +0300

    Enhance file input handling with console logs
    Added console logs for file input and change event.

--- script.js
+++ script.js
@@ -3,10 +3,16 @@ alert("script.js loaded");
 const fileInput = document.getElementById("csvFile");
 const fileName = document.getElementById("fileName");
 
+console.log(fileInput);
+
 fileInput.addEventListener("change", function () {
 
-    alert("File selected!");
+    alert("change event");
 
-    fileName.textContent = this.files[0].name;
+    console.log(this.files);
+
+    if (this.files.length > 0) {
+        fileName.textContent = this.files[0].name;
+    }
 
 });
